71bcb2a1366a392cd4a7f8da7bb5ec3abec27a84
Aaron <Aaron@DESKTOP-BULPHRT>
2016-06-25 16:59:00 +0800

    fff

--- CRM.Core/CRM.Common/CacheHelper.cs
+++ CRM.Core/CRM.Common/CacheHelper.cs
@@ -10,11 +10,11 @@ namespace CRM.Common
     public static class CacheHelper
     {
         //单例模式
-        public static readonly Memcached.ClientLibrary.MemcachedClient mc;
+        public static readonly MemcachedClient mc;
 
         static CacheHelper()
         {
-            string[] servers = { "192.168.1.100:11211" };
+            string[] servers = { "127.0.0.1:11211" };
 
             //初始化池 
             SockIOPool pool = SockIOPool.GetInstance();
@@ -28,21 +28,27 @@ namespace CRM.Common
             pool.Failover = true;
             pool.Nagle = false;
             pool.Initialize();
-            MemcachedClient mc = new Memcached.ClientLibrary.MemcachedClient();
+            mc = new MemcachedClient();
             mc.EnableCompression = false;
         }
 
 
-        public static  void Add(string key, object value, DateTime ExpireDate)
+        public static  void Add(string key, string value, DateTime ExpireDate)
         {
             if (mc.KeyExists(key))
             {
-                mc.Set(key, value, ExpireDate);
+                mc.Set(key, value,ExpireDate);
             }
             else
             {
                 mc.Add(key, value, ExpireDate);
             }
+        }
+        public static void Add(string key, string value, TimeSpan ExpireDate)
+        {
+            DateTime now = DateTime.Now;
+            var ex = now.AddTicks(ExpireDate.Ticks);
+            Add(key, value,ex);
         }
 
         public static object Set(string key)
@@ -52,7 +58,7 @@ namespace CRM.Common
 
         public static void Add<T>(string key, T value, DateTime ExpireDate)
         {
-            Add(key, value, ExpireDate);
+            Add(key, value.ToString(), ExpireDate);
         }
 
         public static T Get<T>(string key) where T : class
